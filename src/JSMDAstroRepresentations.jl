0533fe07660e1c5c25e8297f74c947754a141306
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-12-10 14:00:31 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.46

---
+++ src/JSMDAstroRepresentations.jl
@@ -0,0 +1,5 @@
+module JSMDAstroRepresentations
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using JSMDAstroRepresentations
+using Test
+
+@testset "JSMDAstroRepresentations.jl" begin
+    # Write your tests here.
+end
