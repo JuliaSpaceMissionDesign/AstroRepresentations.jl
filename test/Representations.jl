76eb83ec93e133c1d8bd3cfd294f6abb2b726809
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-12-12 21:00:38 +0100

    update tests, basic representations

--- test/Representations.jl
+++
@@ -1,8 +0,0 @@
-using AstroRepresentations
-using Test
-using ForwardDiff
-using StaticArrays
-using LinearAlgebra
-
-include("repr/keplerian.jl")
-include("repr/equinoctial.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,10 @@
-
+using AstroRepresentations
 using Test
+using ForwardDiff
+using StaticArrays
+using LinearAlgebra
 
-const GROUP = get(ENV, "GROUP", "All")
-const TEST_GROUPS = ("Representations", )
-
-const TEST_TIME = Dict{String, Float64}()
-
-for tg in TEST_GROUPS
-    if GROUP == tg || GROUP == "All"
-        TEST_TIME[tg] = @elapsed begin 
-            @testset "$tg" verbose=true begin 
-                include("$tg.jl")
-            end
-        end
-    end
-end
-
-println("\033[1mTest Execution Time Symmary:\033[0m")
-
-for pair in TEST_TIME 
-    println("Group: $(pair[1]), elapsed time $(pair[2]) s.")
-end
+@testset "Representations.jl" verbose=true begin
+    include("src/keplerian.jl")
+    include("src/equinoctial.jl")
+end;
